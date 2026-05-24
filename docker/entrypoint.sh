0860d936e680d75640a407290af8f54c9c40fe69
Assaf Berg <asssaf@users.noreply.github.com>
2026-05-24 16:56:52 +0000

    remove secret service as it is not needed in the latest version of the cli (using file based storage)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,4 @@
 
 set -euo pipefail
 
-mkdir -p ~/.local/share/keyrings
-export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --session --print-address --fork)
-
-# Force initialization of the default collection with a dummy password
-export $(echo -n "dummy" | gnome-keyring-daemon --unlock)
-
-gnome-keyring-daemon --start --components=secrets
-
 exec /antigravity $*
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,6 @@ set -eu
 # run.sh --prompt "what is the weather in SF?"
 
 mkdir -p "${HOME}/.gemini"
-mkdir -p "${HOME}/.gemini-keyrings"
 mkdir -p "${HOME}/.config/configstore/update-notifier-\@google"
 
 : ${IMAGE:=ghcr.io/asssaf/antigravity-cli:latest}
@@ -17,7 +16,6 @@ mkdir -p "${HOME}/.config/configstore/update-notifier-\@google"
 docker run --rm -it \
         -v ${HOME}/.config/configstore/update-notifier-@google:/home/${GUEST_USER}/.config/configstore/update-notifier-@google \
         -v ${HOME}/.gemini:/home/${GUEST_USER}/.gemini \
-        -v ${HOME}/.gemini-keyrings:/home/${GUEST_USER}/.local/share/keyrings \
         -v ${PWD}:/home/${GUEST_USER}/work \
         -u $(id -u):$(id -g) \
         -e NO_BROWSER=true \
